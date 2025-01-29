fd8eb1b310826a67757c21e8a3a2c955533494fd
Abhinavlather2004 <latherabhinav55@gmail.com>
2025-01-29 11:23:57 +0530

    [Abhinav]ADD: added functionality of user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # Ensure only modern browsers are supported (for web-based apps)
   allow_browser versions: :modern
+
+  # # Apply authentication for API requests
+  # before_action :authenticate_request
+
+  # def authenticate_request
+  #   header = request.headers['Authorization']
+  #   return render json: { error: 'Token missing' }, status: :unauthorized unless header
+
+  #   token = header.split(' ').last
+  #   decoded = JsonWebToken.decode(token)
+
+  #   if decoded
+  #     @current_user = User.find(decoded[:user_id])
+  #   else
+  #     render json: { error: 'Invalid token' }, status: :unauthorized
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/users', to: 'users#createUser'
+      post '/users/login', to: 'users#userLogin'
     end
   end
 end
+
